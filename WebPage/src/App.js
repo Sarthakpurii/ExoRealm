03a825e308e9646cb0bea62877e7937d23a4e9d9
Aakash <vermaaakash1000@gmail.com>
2023-10-08 00:13:43 +0530

    Model Route Updated

--- WebPage/src/App.js
+++ WebPage/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import NavBar from './components/NavBar';
 import BgVideo from "./components/BgVideo";
 import Overview from './components/Overview'
-
+import Model from './components/Model'
 function App() {
   return(
     <div className="App">
       <BgVideo />
       <Overview/>
-
+      <Model/>
     </div>
   );
 }
