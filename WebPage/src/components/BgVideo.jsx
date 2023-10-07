eea99208eb75a84ca14680496a46acaaab0f9f7f
Aakash <vermaaakash1000@gmail.com>
2023-10-07 12:46:07 +0530

    Home Page updated

--- WebPage/src/components/BgVideo.jsx
+++ WebPage/src/components/BgVideo.jsx
@@ -6,9 +6,15 @@ function BgVideo() {
         <div className="bgContainer">
             <div className="overlay">
                 <video src={video} autoPlay loop muted />
-                <div className="flex container flex-row border">
-                    <h2 className="wlc ">WELCOME EXORIANS!</h2>
-                    <h1 className="title">TRAVEL ON YOUR EXOPLANETS</h1>
+                <div className="flex h-full w-full flex-row absolute top-0 left-0 text-white ">
+                    <div className='w-4/6 flex flex-col justify-center p-10'>
+                        <div className='text-justify text-left ml-36  w-[540px]'>
+                            <h2 className="wlc font-bellefair text-[100px] mt-28 leading-none self-center  ">Hey Exorians!</h2>
+                            <p className='font-barlow p-2 text-[#D0D6F9] text-xl text-left relative indent-0'>Are you ready to embark on a journey through the vast expanse of the cosmos? Join us as we dive into the intriguing world of exoplanets, celestial bodies beyond our solar system</p>
+                            {/* <h1 className="title w-1/2">TRAVEL ON YOUR EXOPLANETS</h1> */}
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
