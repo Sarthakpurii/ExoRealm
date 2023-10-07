0be2cf1a5daf5b00b8a276aba01e33f5e7eb20bf
Aakash <vermaaakash1000@gmail.com>
2023-10-07 11:00:30 +0530

    Taiwind setup

--- WebPage/src/components/BgVideo.jsx
+++ WebPage/src/components/BgVideo.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import video from '../assets/video.mp4';
 
-function BgVideo(){
-    return(
+function BgVideo() {
+    return (
         <div className="bgContainer">
             <div className="overlay">
                 <video src={video} autoPlay loop muted />
                 <div className="container">
-                    <h2 className="wlc">WELCOME EXORIANS!</h2>
+                    <h2 className="wlc border">WELCOME EXORIANS!</h2>
                     <h1 className="title">TRAVEL ON YOUR EXOPLANETS</h1>
                 </div>
             </div>
