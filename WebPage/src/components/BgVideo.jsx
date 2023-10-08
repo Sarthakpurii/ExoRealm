ef40aa75c21459f18c455d53e590b719b56d3e89
Aakash <vermaaakash1000@gmail.com>
2023-10-08 15:07:21 +0530

    Routes Updated

--- WebPage/src/components/BgVideo.jsx
+++ WebPage/src/components/BgVideo.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import video from '../assets/video.mp4';
-import NavBar from './NavBar';
 
 function BgVideo() {
-    const scrollToSection = (sectionId) => {
-        const section = document.getElementById(sectionId);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
     return (
-        <div id="home" className="h-screen flex flex-col items-center -z-10">
-
-            <div className='fixed self-center w-[465px] h-20'><NavBar /></div>
+        <div id="home" className="h-screen flex flex-col items-center">
             <video src={video} autoPlay loop muted />
-            <div className="flex h-[80vh] mt-20 w-full flex-row absolute top-0 justify-center text-white ">
+            <div className="flex h-[80vh] mt-20 w-full flex-row absolute top-0 justify-center text-white z-10">
                 <div className='flex flex-col justify-center items-center p-10 gap-5'>
                     <div className='w-[540px]'>
                         <h2 className="font-bellefair text-[100px] leading-none">Hey Exorians!</h2>
@@ -23,7 +14,7 @@ function BgVideo() {
                             Lets delve into in the world to embark on a voyage of boundless creativity and exploration. Step into the universe of ExoRealm, where the cosmos becomes your canvas and your imagination knows no bounds. Join us on a thrilling quest to create and explore exoplanets of your very own making. Get Ready for the journey to Comsmos!</p>
 
                     </div>
-                    <button onClick={() => scrollToSection('model')} className='hover:w-40 hover:h-40 rounded-full w-28 h-28 bg-slate-100 text-black font-barlowc text-2xl'>Explore</button>
+                    <NavLink to='game'><button className='hover:w-40 hover:h-40 rounded-full w-28 h-28 bg-slate-100 text-black font-barlowc text-2xl'>Play Game</button></NavLink>
                 </div>
             </div>
 
--- WebPage/src/components/Model.jsx
+++ WebPage/src/components/Model.jsx
@@ -1,12 +1,7 @@
 import React, { Suspense } from 'react'
-import { Canvas, extend } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import styled from 'styled-components'
 import { Earth } from '../components/earth'
-
-
-extend({ OrbitControls, Canvas })
-
 
 
 const CanvasContainer = styled.div`
@@ -17,7 +12,7 @@ const CanvasContainer = styled.div`
 export default function Model() {
   return (
     <div id='demo' className='-z-10 h-screen w-full '>
-      <div className='absolute top-[200vh] h-[80vh] mt-[20vh] text-white  left-[50vw] z-10'>
+      <div className='absolute top-[0] h-[80vh] mt-[20vh] text-white  left-[60vw] z-10'>
         {/* Try out your hands on the 3D model and dive into the thrilling experience of an unraveling journey of ExoRealm. */}
         <h2 className='font-bellefair text-[100px] leading-none'>51 Eridani B</h2>
         <p className='font-barlow p-2 text-[#D0D6F9] text-xl w-[500px]'>It is 96 light years away from the solar system, and it is approximately 20 million years old. It's an gas giant exoplanet that orbits an F-type star. Its mass is 2 Jupiters, it takes 32 years to complete one orbit of its star, and is 13.2 AU from its star.</p>
@@ -25,7 +20,7 @@ export default function Model() {
         <span className='flex flex-row gap-10 justify-between w-[450px] mt-5 ml-3  font-barlowc'><p className=' text-[#D0D6F9]'>AVG. DISTANCE<br/>96 Light Years</p> <p className='text-[#D0D6F9]'>DISCOVERY DATE<br/>2015</p></span>
 
       </div>
-      <CanvasContainer className='h-screen w-full z-[-10]'>
+      <CanvasContainer className='h-screen w-full '>
         <Canvas>
           <Suspense fallback={null}>
             <Earth />
