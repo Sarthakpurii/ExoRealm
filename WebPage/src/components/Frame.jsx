40c2deb294824a9b551203e006487dfd6bb6d3fd
Aakash <vermaaakash1000@gmail.com>
2023-10-08 10:37:56 +0530

    Frame Page Updated with Unity

--- WebPage/src/components/Frame.jsx
+++ WebPage/src/components/Frame.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { Unity, useUnityContext } from "react-unity-webgl";
 function Frame() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/WebGL_Build.loader.js",
+    dataUrl: "build/webgl.data",
+    frameworkUrl: "build/build.framework.js",
+    codeUrl: "build/build.wasm",
+  });
+
   return (
-    <div id='model' className='h-screen w-full'>Frame</div>
+    <div id='model' className='h-screen w-full'>
+      <Unity style={{ width: '100%', height: "100%" }} unityProvider={unityProvider} />
+    </div>
   )
 }
 
